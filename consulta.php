e8be53abe6114e20b7c5f3224b100af505b9233f
zayymr <zairawydks@gmail.com>
2018-05-22 15:50:56 -0600

    Completado

---
+++ consulta.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> Consulta </title> 
+<link rel="stylesheet" type="text/css"  href="css/registro.css">
+<meta charset="utf-8">
+<script src="js/jquery-3.3.1.js"></script>
+<script src="js/consulta_ajax.js"></script>
+</head>
+<body>
+<div id="div1"><h2>Muestra los registros realizados ordenados por apellido paterno</h2></div><br>
+<button id="boton">Realizar consulta</button>
+</body>
+</html>
