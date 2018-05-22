965f3cbae99a036551386e34e04ed0ff2ac2f1cc
zayymr <zairawydks@gmail.com>
2018-05-22 15:51:25 -0600

    Completado

---
+++ creditos.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css"  href="css/registro.css">
+<title> Créditos </title> 
+</head>
+<body>
+<h2><center>Créditos</center></h2>
+<div>
+<img src="/images/unam.jpg" alt="UNAM LOGO" style="float:left;width:200px;height:200px;">
+<img src="/images/fca.jpg" alt="FCA LOGO" style="float:right;width:200px;height:200px;">
+<center>Mtro. Tomás Humberto Rubio Pérez<br></center>
+<center>Director<br><br></center>
+<center>Dr. Armando Tomé González<br></center>
+<center>Secretario General<br><br></center>
+<center>Mtro. Balfred Santaella Hinojosa<br></center>
+<center>Jefe del Centro de Informática<br><br></center>
+<center>Zayra Molina Ramírez<br></center>
+<center>Responsable de Diseño y Desarrollo Web<br></center>
+</div><br>
+<form action="http://www.3pzmolina.unam.mx">
+<a href="http://www.3pzmolina.unam.mx/consulta.php">Consulta</a> |  <a href="http://www.3pzmolina.unam.mx/contacto.php">Contacto</a> | <a href="http://www.3pzmolina.unam.mx/creditos.php">Créditos</a>
+<input type="submit" value="Regresar al formulario" />
+</form>
+</body>
+</html>
