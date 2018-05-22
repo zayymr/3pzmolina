c01feb1d695594330da455a8e097f33a7e655e21
zayymr <zairawydks@gmail.com>
2018-05-22 15:50:02 -0600

    Completado

--- contacto.php
+++ contacto.php
@@ -4,10 +4,14 @@
 <title> Contacto </title> 
 </head>
 <body>
-<h2><center>Queremos saber tu opinion</center></h2>
+<h2><center>Queremos saber tu opinión</center></h2>
 <div><center> Gracias por entrar a nuestro sitio, para mejorar tu experiencia envianos un correo con tus comentarios y sugerencias.</center><br>
 <center><a href="mailto:name@rapidtables.com?subject=Comentarios%20y%20sugerencias">Manda un correo</a></center>
-</div>
+</div><br>
+<form action="http://www.3pzmolina.unam.mx">
+<a href="http://www.3pzmolina.unam.mx/consulta.php">Consulta</a> |  <a href="http://www.3pzmolina.unam.mx/contacto.php">Contacto</a> | <a href="http://www.3pzmolina.unam.mx/creditos.php">Créditos</a>
+<input type="submit" value="Regresar al formulario" />
+</form>
 </body>
 </html>
 
