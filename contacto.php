1e0f5dff67fbc344b91c10071ad4ae4e600fbd7d
zayymr <zairawydks@gmail.com>
2018-05-22 08:12:59 -0600

    3er parcial

---
+++ contacto.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css"  href="css/registro.css">
+<title> Contacto </title> 
+</head>
+<body>
+<h2><center>Queremos saber tu opinion</center></h2>
+<div><center> Gracias por entrar a nuestro sitio, para mejorar tu experiencia envianos un correo con tus comentarios y sugerencias.</center><br>
+<center><a href="mailto:name@rapidtables.com?subject=Comentarios%20y%20sugerencias">Manda un correo</a></center>
+</div>
+</body>
+</html>
+
